9777d35b6ba57c104f81ff41220ec043ea1ccd43
CrioUser <crio-user@criodo.com>
2021-01-29 15:00:08 +0530

    try 4

--- prepare-server.sh
+++ prepare-server.sh
@@ -10,13 +10,13 @@ GRADLE_OPTS="-Dgradle.user.home=~/gradle_cache"    #
 
 
 ./gradlew clean bootrun &
-
+mongoimport --uri mongodb+srv://admin:adminP%40ssw0rd@buildout-qa.esf6i.mongodb.net/test --collection questions --type json --file initial_data_load.json
 while ! netstat -tna | grep 'LISTEN\>' | grep -q ':8081\>'; do
   echo "waiting for spring application to start"
   sleep 3 # time in seconds, tune it as needed
 done
 
-mongoimport --uri mongodb+srv://admin:adminP%40ssw0rd@buildout-qa.esf6i.mongodb.net/test --collection questions --type json --file initial_data_load.json
+
 #mongoimport --uri mongodb+srv://mongodb+srv://admin:Anand123@buildout-qa.94q9m.mongodb.net/test --collection questions --type json --file initial_data_load.json
 
 #If you have any script to load the data make sure that its part of this bash script.
